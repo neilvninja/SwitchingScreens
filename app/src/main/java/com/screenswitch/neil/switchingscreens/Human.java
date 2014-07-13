22b434328e1b52dabfe493d259eeac77ee55ef66
neil <neilvninja@yahoo.co.uk>
2014-07-13 18:51:47 +0100

    final program update

--- app/src/main/java/com/screenswitch/neil/switchingscreens/Human.java
+++ app/src/main/java/com/screenswitch/neil/switchingscreens/Human.java
@@ -3,4 +3,37 @@ package com.screenswitch.neil.switchingscreens;
 import java.io.Serializable;
 
 public class Human implements Serializable { //stating that this class can be sent ot other services
+
+    private double height, weight;
+    private String name = "";
+
+    public Human(double height, double weight, String name) {
+        this.height = height;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
