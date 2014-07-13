156b454620645a5830ed8c77aedf8c83178d5e42
neil <neilvninja@yahoo.co.uk>
2014-07-13 18:28:12 +0100

    final program update

---
+++ app/src/main/java/com/screenswitch/neil/switchingscreens/Human.java
@@ -0,0 +1,6 @@
+package com.screenswitch.neil.switchingscreens;
+
+import java.io.Serializable;
+
+public class Human implements Serializable { //stating that this class can be sent ot other services
+}
