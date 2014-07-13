a5a0f37009bfa96b0ce4d02db7c98a4a974e9c97
neil <neilvninja@yahoo.co.uk>
2014-07-13 14:20:27 +0100

    Initial commit

---
+++ app/src/main/java/com/screenswitch/neil/switchingscreens/SecondScreen.java
@@ -0,0 +1,12 @@
+package com.screenswitch.neil.switchingscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondScreen extends Activity { //as it is an activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
